8d68b2f160deec302dbbae0b3d28139b2b4b0414
vijay11234 <victory6923@gmail.com>
2020-09-25 23:18:46 +0530

    Monthly salary of an Employee

---
+++ MonthlyWage_ofEmployee.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -x
+isPresent=1
+ispartTime=2
+EmployeeCheck=$(( RANDOM%3 ))
+wageperHour=20
+Totaworkingdays=20
+    case $EmployeeCheck in
+        $isPresent)
+           echo "Employee is present $isPresent"
+         	workhrs=8
+            dailywage=$((wageperHour*workhrs))
+        		echo "$dailywage"
+                ;;
+        $ispartTime)
+      		echo "Employee is par-time"
+         	workhrs=4
+         	dailywage=$((wageperHour*workhrs))
+        		echo "$dailywage"
+                ;;
+         *)
+                echo "Employee is absent"
+	 esac
+  Salary=$((dailywage*Totaworkingdays))
+	echo "$Salary"
